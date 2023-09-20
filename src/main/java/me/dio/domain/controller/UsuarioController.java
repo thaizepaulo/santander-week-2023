44d04af39c8b47c0e8ae86ff90ee36282ed69cf8
Thaize Paulo <thaizepaulo@gmail.com>
2023-09-20 11:57:41 -0300

    Implementando classes de Controle

---
+++ src/main/java/me/dio/domain/controller/UsuarioController.java
@@ -0,0 +1,38 @@
+package me.dio.domain.controller;
+
+import me.dio.domain.model.Usuario;
+import me.dio.domain.service.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable Long id) {
+        Usuario usuario = usuarioService.findById(id);
+        return ResponseEntity.ok(usuario);
+    }
+
+
+    @PostMapping()
+    public ResponseEntity<Usuario> findById(@RequestBody Usuario usuario) {
+        Usuario usuarioCriado = usuarioService.create(usuario);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("{id}")
+                .buildAndExpand(usuarioCriado.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(usuarioCriado);
+    }
+
+}
