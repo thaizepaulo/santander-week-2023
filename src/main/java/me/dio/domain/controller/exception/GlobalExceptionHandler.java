b30c59f015db924146eacc7a72c5203da6c79ec5
Thaize Paulo <thaizepaulo@gmail.com>
2023-09-20 14:34:16 -0300

    Tratamento de Exceções

---
+++ src/main/java/me/dio/domain/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package me.dio.domain.controller.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException) {
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException) {
+        return new ResponseEntity<>("Id do Recurso Não Encontrado.", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException) {
+        var mensagem = "Erro de servidor não esperado, veja os logs.";
+        logger.error(mensagem, unexpectedException);
+        return new ResponseEntity<>(mensagem, HttpStatus.NOT_FOUND);
+    }
+}
