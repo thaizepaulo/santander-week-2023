27380c79754205f1b26d76b1d1b9f03e7b4254e2
Thaize Paulo <thaizepaulo@gmail.com>
2023-09-20 09:41:57 -0300

    Initial commit

---
+++ src/test/java/me/dio/JavaDioSantanderWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDioSantanderWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
